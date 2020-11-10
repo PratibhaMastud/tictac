27e2ecba9428c13b906738b6e2d87a1c986e3495
PratibhaMastud <karandepratibha999@gmail.com>
2020-11-10 19:43:11 +0530

    Added:Uc-2

--- TicTacGame.java
+++ TicTacGame.java
@@ -1,28 +1,32 @@
 import java.util.Scanner;
 
-class TicTac{
+class TicTacGame{
 
-        public static void main(String[] args){
+ public static void main(String[] args){
 
+                char[] game = startGame();
                 Scanner sc=new Scanner(System.in);
                 System.out.println("Enter 1 or 2");
                 int in=sc.nextInt();
-		
-		char[] game = startGame();
 
-
+                allowInput();
         }
-// Uc-1
-        public static char[] startGame(){
 
-                char[] game = new char[10];
+	public static void allowInput(){
+		System.out.println("Enter Input as 'x' or 'o' ");
+		char input=sc.next().charAt(0);
+		switch(input){
+			case 'x':
+				User='x';
+				opponent='o';
+				break;
+			case 'o':
+				User='o';
+				opponent='x';
+				break;
+			default:
+				break;
+		}
 
-                for(int j=0; j<9 ; j++){
-
-                        game[j]=' ';
-
-
-                }
-                return game;
-        }
+	}
 }
