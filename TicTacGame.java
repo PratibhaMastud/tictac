db9236dab13d51790221e6e2032292ed1b6e17cc
PratibhaMastud <karandepratibha999@gmail.com>
2020-11-10 19:50:11 +0530

    Added:Display Method

--- TicTacGame.java
+++ TicTacGame.java
@@ -1,32 +1,40 @@
 import java.util.Scanner;
 
-class TicTacGame{
+class TicTac{
 
- public static void main(String[] args){
+        public static void main(String[] args){
 
-                char[] game = startGame();
                 Scanner sc=new Scanner(System.in);
                 System.out.println("Enter 1 or 2");
                 int in=sc.nextInt();
+                char[] game = startGame();
+		showBoard(game);
+                
+        }
+	 public static char[] startGame(){
 
-                allowInput();
+                char[] game = new char[10];
+
+                for(int j=0; j<10 ; j++){
+
+                        game[j]=' ';
+
+
+                }
+                return game;
         }
 
-	public static void allowInput(){
-		System.out.println("Enter Input as 'x' or 'o' ");
-		char input=sc.next().charAt(0);
-		switch(input){
-			case 'x':
-				User='x';
-				opponent='o';
-				break;
-			case 'o':
-				User='o';
-				opponent='x';
-				break;
-			default:
-				break;
-		}
 
-	}
+	public static void showBoard(char[] game){
+
+                System.out.println("----|---|----");
+                System.out.println("| " + game[0] + " | " + game[1] + " | " + game[2] + " |");
+                System.out.println("|-----------|");
+                System.out.println("| " + game[3] + " | " + game[4] + " | " + game[5] + " |");
+                System.out.println("|-----------|");
+                System.out.println("| " + game[6] + " | " + game[7] + " | " + game[8] + " |");
+                System.out.println("----|---|----");
+
+
+        }
 }
